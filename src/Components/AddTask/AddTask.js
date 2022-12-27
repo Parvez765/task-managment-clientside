b9969f1f53fa2f48da0812a83ebf66f3b7029d64
Pervez <pervezhossain1998@gmail.com>
2022-12-27 12:33:22 +0600

    Post Api Created For Task

--- src/Components/AddTask/AddTask.js
+++ src/Components/AddTask/AddTask.js
@@ -1,9 +1,75 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 
 const AddTask = () => {
+
+    const handleOnSubmit = event => {
+        event.preventDefault()
+        const form = event.target
+        const task = form.task.value
+        const taskDescription = form.taskDescription.value
+        // console.log(task, taskDescription)
+        const image = form.img.files[0]
+
+        const imgbbKey = process.env.REACT_APP_imgbbKey
+
+        const formData = new FormData()
+        formData.append("image", image)
+
+        fetch(`https://api.imgbb.com/1/upload?key=${imgbbKey}`, {
+            method: "POST",
+            body: formData
+        })
+            .then(res => res.json())
+            .then(imgdata => {
+                if (imgdata.success || !imgdata.success) {
+                    const taskdetails = { task, taskDescription, imgdata }
+                    fetch(`http://localhost:5000/task`, {
+                        method: "POST",
+                        headers: {
+                            "content-type" : "application/json"
+                        },
+                        body : JSON.stringify(taskdetails)
+                    })
+                        .then(res => res.json())
+                        .then(data => {
+                            console.log(data)
+                            if (data.acknowledged) {
+                                Swal.fire(
+                                    'Task Addedd Successfully!',
+                                    'Thank You',
+                                    'success'
+                                  )
+                           }
+                            
+                    })
+                }
+            })
+        form.reset()
+    }
+
     return (
         <div>
-            <h2>Hello From Add Task</h2>
+            <h2 className='mt-5 mb-5 text-center'>Add Your Task</h2>
+            <form className="row g-3 container mx-auto" onSubmit={handleOnSubmit}>
+                <div className="col-md-6">
+                    <label className="form-label">Task Name</label>
+                    <input type="text" name="task" className="form-control" placeholder='Enter Your Task Name' required/>
+                </div>
+                <div className="col-md-6">
+                <label className="form-label">Upload Images (Optional)</label>
+                    <div className="mb-3">
+                       <input className="form-control" type="file" name="img" id="formFile"/>
+                    </div>
+                </div>
+                <div className=" col-12 lg-col-6 ">
+                    <label className="form-label">Task Description</label><br/>
+                    <textarea class="form-control" name="taskDescription" id="exampleFormControlTextarea1" rows="3" placeholder='Enter Task Description'></textarea>
+                </div>
+                <div className="col-12">
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </div>
+            </form>
         </div>
     );
 };
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h2>Hello From Home</h2>
+            <h2 className='text-center'>Hello From Home</h2>
         </div>
     );
 };
